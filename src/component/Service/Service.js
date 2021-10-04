40315182c2c6b8e842186694ce318c73fb82293a
sadrulj <jamil.sadrul@gmail.com>
2021-10-05 05:37:34 +0600

    fontawesom and rating

--- src/component/Service/Service.js
+++ src/component/Service/Service.js
@@ -1,21 +1,40 @@
 import React from "react";
 import { Badge, Card, Col } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBook, faStar } from "@fortawesome/free-solid-svg-icons";
 
 const service = (props) => {
-  const { badge, image, lesson, title, imageInstructer, instructer, price } =
-    props.data;
+  const {
+    badge,
+    image,
+    lesson,
+    title,
+    imageInstructer,
+    instructer,
+    price,
+    rating,
+  } = props.data;
   return (
     <Col>
       <Card className="shadow mb-4">
         <Card.Img variant="top" style={{ height: "200px" }} src={image} />
         <Card.ImgOverlay>
           <div className="d-flex justify-content-start">
-            <Badge bg="warning">{badge}</Badge>{" "}
+            <Badge bg="warning">{badge}</Badge>
           </div>
         </Card.ImgOverlay>
         <Card.Body className="text-start my-3">
-          <Card.Title>{lesson} Lesson</Card.Title>
-          <Card.Title>{title}</Card.Title>
+          <Card.Title className="d-flex justify-content-between">
+            <small>
+              <FontAwesomeIcon className="me-2" icon={faBook} />
+              {lesson} Lesson
+            </small>
+            <small>
+              <FontAwesomeIcon style={{ color: "goldenrod" }} icon={faStar} />
+              {rating}
+            </small>
+          </Card.Title>
+          <Card.Title className="my-4">{title}</Card.Title>
           <Card.Text className="d-flex justify-content-start">
             <Card.Img
               variant="rounded"
